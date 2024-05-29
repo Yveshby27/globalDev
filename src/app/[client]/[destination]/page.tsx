6436d725c8705817cb3589151ff210341330622f
Yves <yves.habchy@gmail.com>
2024-05-29 22:48:17 +0300

    Render the country details component on the server

--- src/app/[client]/[destination]/page.tsx
+++ src/app/[client]/[destination]/page.tsx
@@ -1,11 +1,16 @@
 import CountryDetails from "~/components/CountryDetails";
-import countryData from "~/data";
-export default function DestinationPage() {
+
+export default function Page({
+  params,
+}: {
+  params: { client: string; destination: string };
+}) {
   return (
     <div>
-      <div className="mt-3 justify-center gap-5">
-        <CountryDetails data={countryData}></CountryDetails>
-      </div>
+      <CountryDetails
+        client={params.client}
+        destination={params.destination}
+      ></CountryDetails>
     </div>
   );
 }
--- src/components/CountryDetails.tsx
+++ src/components/CountryDetails.tsx
@@ -1,69 +1,39 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import { useInfoContext } from "~/app/context";
+import countryData from "~/data";
 
 interface DataProps {
-  name: string;
-  average_salary: number;
-  description: string;
-  zone: string;
-  std: number;
+  client: string;
+  destination: string;
 }
 
-const CountryDetails = ({ data }: { data: DataProps[] }) => {
-  const context = useInfoContext();
-  const [country1, setCountry1] = useState<DataProps | null>(null);
-  const [country2, setCountry2] = useState<DataProps | null>(null);
-  const [comparisonMessage, setComparisonMessage] = useState<string | null>(
-    null,
-  );
-
-  useEffect(() => {
-    for (const country of data) {
-      if (context.clientCountry === country.name) setCountry1(country);
-      if (context.destinationCountry === country.name) setCountry2(country);
+const CountryDetails: React.FC<DataProps> = ({ client, destination }) => {
+  const getCountry = (countryName: string) => {
+    for (const country of countryData) {
+      if (country.name === countryName) return country;
     }
-  }, [context]);
+  };
+  const country1 = getCountry(client);
+  const country2 = getCountry(destination);
+  if (!country1 || !country2) return;
+  const percentageDiff =
+    ((country2.average_salary - country1.average_salary) /
+      country1.average_salary) *
+    100;
 
-  useEffect(() => {
-    if (country1 && country2) {
-      const percentageDiff =
-        ((country2.average_salary - country1.average_salary) /
-          country1.average_salary) *
-        100;
-      const higherCountry =
-        country2.average_salary > country1.average_salary
-          ? country2.name
-          : country1.name;
-      const timezoneDifference = country2.std - country1.std;
-      const direction =
-        timezoneDifference > 0
-          ? "ahead"
-          : timezoneDifference < 0
-            ? "behind"
-            : "the same as";
-      const absoluteDifference = Math.abs(timezoneDifference);
-      if (absoluteDifference !== 0) {
-        setComparisonMessage(
-          `Salary in ${higherCountry} is ${Math.abs(
-            parseFloat(percentageDiff.toFixed(2)),
-          )}% higher than ${
-            higherCountry === country2.name ? country1.name : country2.name
-          }. ${country2.name} is ${absoluteDifference} hours ${direction} ${
-            country1.name
-          } in terms of timezone.`,
-        );
-      } else {
-        setComparisonMessage(
-          `Salary in ${higherCountry} is ${Math.abs(
-            parseFloat(percentageDiff.toFixed(2)),
-          )}% higher than ${
-            higherCountry === country2.name ? country1.name : country2.name
-          }.${country1.name} is the same as ${country2.name} in terms of timezone.`,
-        );
-      }
-    }
-  }, [country1, country2]);
+  const higherCountry =
+    country2.average_salary > country1.average_salary
+      ? country2.name
+      : country1.name;
+
+  const timezoneDifference = country2.std - country1.std;
+
+  const direction =
+    timezoneDifference > 0
+      ? "ahead"
+      : timezoneDifference < 0
+        ? "behind"
+        : "the same as";
+
+  const absoluteDifference = Math.abs(timezoneDifference);
 
   return (
     <div>
@@ -73,12 +43,26 @@ const CountryDetails = ({ data }: { data: DataProps[] }) => {
           <p className="mt-2">{country1.description}</p>
           <h2 className="text-xl font-semibold">{country2.name}</h2>
           <p className="mt-2">{country2.description}</p>
-          {comparisonMessage && <p className="mt-2">{comparisonMessage}</p>}
         </div>
       )}
-      <button className="mt-4 max-h-14 bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
-        More details
-      </button>
+      {absoluteDifference !== 0 && (
+        <div>
+          {" "}
+          Salary in {higherCountry} is{" "}
+          {Math.abs(parseFloat(percentageDiff.toFixed(2)))}% higher than{" "}
+          {higherCountry === country2.name ? country1.name : country2.name}.{" "}
+          {country2.name} is {absoluteDifference} hours {direction}{" "}
+          {country1.name} in terms of timezone.
+        </div>
+      )}
+      {absoluteDifference === 0 && (
+        <div>
+          Salary in {higherCountry} is{" "}
+          {Math.abs(parseFloat(percentageDiff.toFixed(2)))}% higher than{" "}
+          {higherCountry === country2.name ? country1.name : country2.name}.
+          {country1.name} is the same as {country2.name} in terms of timezone.
+        </div>
+      )}
     </div>
   );
 };
