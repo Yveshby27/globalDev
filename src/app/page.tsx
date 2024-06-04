c67c3c9f6618ba738de6aab948a2be415c4fe5a1
Yves <yves.habchy@gmail.com>
2024-06-04 10:24:03 +0300

    Center the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import ClientCountryDropdown from "~/components/ClientCountryDropdown";
 
 export default function MainPage() {
   return (
-    <div className="mt-5">
-      <label className="mb-2">Where are you based in?</label>
-      <ClientCountryDropdown data={countryData}></ClientCountryDropdown>
-      <div className="flex w-full items-center justify-center">
-        <div className="mt-6 flex w-7/12 min-w-96 items-center justify-center">
-          <WorldMap></WorldMap>
+    <div className="mt-5 flex justify-center">
+      <div>
+        <label className="mb-2">Where are you based in?</label>
+        <ClientCountryDropdown data={countryData}></ClientCountryDropdown>
+        <div className="flex w-full items-center justify-center">
+          <div className="mt-10 flex min-w-96 items-center justify-center">
+            <WorldMap></WorldMap>
+          </div>
         </div>
       </div>
     </div>
